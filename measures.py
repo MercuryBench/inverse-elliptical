30f2be4770316bfa486d4c67bccd436ad6bc75a4
Philipp Wacker <wacker@fauamlx4a.am.uni-erlangen.de>
2017-03-17 14:28:55 +0100

    raw measure class with abstract base class 'measure'

---
+++ measures.py
@@ -0,0 +1,23 @@
+from __future__ import division
+from abc import ABCMeta
+import numpy as np
+
+class measure:
+	__metaclass__ = ABCMeta
+	
+	@abstractmethod
+	def sample(self):
+		raise NotImplementedError()
+		
+	@abstractproperty
+	def mean(self):
+		raise NotImplementedError()
+	
+	@abstractproperty
+	def gaussApprox(self):
+		raise NotImplementedError()
+
+class Gaussian(measure):
+	pass
+	
+	
