fa7295213911bc6ebcd246f20a27317c4f236e61
Philipp Wacker <wacker@fauamlx4a.am.uni-erlangen.de>
2017-03-17 14:14:27 +0100

    class for forward problem created, can solve elliptical forward problem now

---
+++ fwdProblem.py
@@ -0,0 +1,19 @@
+from __future__ import division
+import numpy as np
+import mapOnInterval as moi
+
+class linEllipt():
+	# model: -(k*p')' = g, with p(0) = pminus, p(1) = pplus, for p
+	def __init__(self, g, pplus, pminus):
+		self.g = g
+		self.pplus = pplus
+		self.pminus = pminus
+
+	def solveLinEllipt(x, k):
+		# solves -(k*p')' = g, with p(0) = pminus, p(1) = pplus, for p
+		I_1 = moi.integrate(x, 1.0/k)
+		I_2 = moi.integrate(x, self.g)
+		I_3 = moi.integrate(x, I_2/k)
+		C = (self.pplus - self.pminus + I_3[-1])/(I_1[-1])
+		p = -I_3 + C*I_1 + self.pminus
+		return p, C
