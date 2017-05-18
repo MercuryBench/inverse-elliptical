e7da757fded8503808cfbe276fcf4ce61b11cee5
Philipp Wacker <phkwacker@gmail.com>
2017-05-18 14:10:37 +0200

    add test routine for fenics forward operator. NOT WORKING: moi.handle() function (gives matrix instead of scalar)

--- poisson_fenics.py
+++ poisson_fenics.py
@@ -9,14 +9,36 @@ Test problem is chosen to give an exact solution at all nodes of the mesh.
 
 from __future__ import print_function
 from fenics import *
+import measures as ms
+import numpy as np
 
 # Create mesh and define function space
-mesh = UnitSquareMesh(8, 8)
+mesh = UnitSquareMesh(16, 16)
 V = FunctionSpace(mesh, 'P', 1)
 
 # Define boundary condition
-u_D = Expression('1 + x[0]*x[0] + 2*x[1]*x[1]', degree=2)
+u_D = Expression('0.01 + x[0]*x[0]', degree=2)
+#u_D = Expression('0', degree=2)
+# Define permeability
+kappa = Expression('x[0] <= 0.5 ? 10 : 1', degree=2)
 
+class myKappa(Expression):
+	def eval(self, values, x):
+		if x[0] <= 0.5:
+			values[0] = 10
+		elif x[0] >= 0.8:
+			values[0] = 10
+		else:
+			values[0] = 1
+
+m = ms.GaussianFourier2d(np.zeros((31,)), 3, 0.5).sample()
+
+class myKappa2(Expression):
+	def eval(self, values, x):
+		values[0] = m.handle(x)
+
+#kappa = Expression('1', degree=2)
+kappa = myKappa2(degree=2)
 def boundary(x, on_boundary):
     return on_boundary
 
@@ -26,7 +48,7 @@ bc = DirichletBC(V, u_D, boundary)
 u = TrialFunction(V)
 v = TestFunction(V)
 f = Constant(-6.0)
-a = dot(grad(u), grad(v))*dx
+a = kappa*dot(grad(u), grad(v))*dx
 L = f*v*dx
 
 # Compute solution
